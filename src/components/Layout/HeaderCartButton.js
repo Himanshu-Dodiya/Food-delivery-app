ea4bd539579df83f25680afd6d3cd28e78c933fe
Himanshu-Dodiya <himanshudodiya26@gmail.com>
2023-04-16 19:08:58 +0530

    bump class added to cart button when item is added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,30 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
+
 const HeaderCartButton = (props) => {
- const cartCtx =  useContext(CartContext);
-const numberOfCartItems = cartCtx.items.reduce((currNumber,item)=>{
-  return currNumber + item.amount;
-},0);
+  const [btnIsHigh, setBtnIsHigh] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHigh(true);
+    const timer = setTimeout(() => {
+      setBtnIsHigh(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+  const btnClasses = `${styles.button} ${btnIsHigh ? styles.bump : ""}`;
   return (
-    <button className={styles.button} onClick= {props.onCartClick}>
+    <button className={btnClasses} onClick={props.onCartClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
